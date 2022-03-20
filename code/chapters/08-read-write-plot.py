bd6a4021d36c777b2541ad1400fa3b71c8e4e68b
Robin Lovelace <rob00x@gmail.com>
2022-03-20 17:03:01 +0000

    Move files for #8, update actions

--- code/chapters/08-read-write-plot.py
+++ code/chapters/08-read-write-plot.py
@@ -7,9 +7,9 @@
 #       format_version: '1.5'
 #       jupytext_version: 1.13.7
 #   kernelspec:
-#     display_name: Python 3 (ipykernel)
+#     display_name: OSMNX
 #     language: python
-#     name: python3
+#     name: osmnx
 # ---
 
 # # Geographic data I/O {#read-write}
