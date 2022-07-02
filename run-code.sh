22fce1b7767a2f301f31f1a825193fe938cf9f86
Robin Lovelace <rob00x@gmail.com>
2022-07-02 12:36:09 +0100

    Use nbconvert for #71

--- run-code.sh
+++ run-code.sh
@@ -1,3 +1,7 @@
 # Aim: run all Python scripts in code folder for testing outputs
-for f in code/chapters/*.py; do python "$f"; done
+
+# These are failing: 
+# for f in code/chapters/*.py; do python "$f"; done
 # for f in ipynb/*.ipynb; do ipython "$f"; done
+
+for f in ipynb/*.ipynb; do jupyter nbconvert --execute --to html "$f"; done
