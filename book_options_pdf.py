a05d4fd83b82bd366ecd2393be54ecd747290fcc
Jakub Nowosad <Nowosad@users.noreply.github.com>
2024-06-11 18:41:59 +0200

    Update book_options_pdf.py

--- book_options_pdf.py
+++ book_options_pdf.py
@@ -1,2 +1,11 @@
+import os
+import matplotlib.pyplot as plt
 import pandas as pd
-pd.options.display.max_colwidth = 13
+pd.set_option('display.max_rows', 6)
+pd.set_option('display.max_columns', 5)
+plt.rcParams['figure.figsize'] = (5, 5)
+
+if os.environ['QUARTO_PANDOC_TO'] == 'html':
+  pd.options.display.max_colwidth = 35
+if os.environ['QUARTO_PANDOC_TO'] == 'latex':
+  pd.options.display.max_colwidth = 13
